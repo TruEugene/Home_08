18f8350bad9fb95c4953c36e45245eddcc67b1e7
Trubnikov Eugene <trubnikov.e@gmail.com>
2023-04-22 20:11:44 +0300

    1

--- test_def.py
+++ test_def.py
@@ -19,12 +19,11 @@ def t_def():
     # Выберем нужные столбцы.
     columns = ['Name', 'Age', 'Pclass']
 
-    # Удалим ненужные столбцы из отфильтрованной таблицы, а также заполним пропуски нулями
+    # Удалим ненужные столбцы из отфильтрованной таблицы
     save_male = pd.DataFrame(data=save_male, columns=columns)
-#    save_male['Age'] = save_male['Age'].fillna(0)
 
     # Вывести Pclass, Name, Age спасённых
-    x, y = st.slider("Задайте диапазон возраста", 0, 120, (30, 60))
+    x, y = st.slider("Задайте диапазон возраста", None, 90, (30, 60))
     save_male_filter = save_male[save_male['Age'].between(x, y)]
     st.write(save_male_filter)
 
