a31cc545ad1766cf518b1cb1e2b34007b73666cc
Trubnikov Eugene <trubnikov.e@gmail.com>
2023-04-23 12:55:39 +0300

    1

--- test_def.py
+++ test_def.py
@@ -5,12 +5,12 @@ import pandas as pd
 def t_def():
     st.text('тестирование')
 
-    #st.header("Список спасённых мужчин среднего возраста от 30 до 60 лет (поля Name, Age, Pclass)")
+    st.header("Список  мужчин среднего возраста от 30 до 60 лет (поля Name, Age, Pclass)")
 
     # Создадим переменную df и запишем в неё содержимое data.csv
     df = pd.read_csv('data.csv')
 
-    # Создадим переменную save_male и запишем в неё отфильтрованную таблицу по значениям из задания.
+    # Создадим переменную male и запишем в неё отфильтрованную таблицу по значениям из задания.
     male = df[df['Sex'] == "male"]
 
     # Выберем нужные столбцы.
@@ -19,7 +19,7 @@ def t_def():
     # Удалим ненужные столбцы из отфильтрованной таблицы
     male = pd.DataFrame(data=male, columns=columns)
 
-    # Вывести Pclass, Name, Age спасённых
+    # Вывести Pclass, Name, Age мужчин
     x, y = st.slider("Задайте диапазон возраста", 0, 90, (30, 60))
     male_filter = male[male['Age'].between(x, y)]
     st.write(male_filter)
