05b213f437c0982228667d4fa126ec5cc380c2df
Trubnikov Eugene <trubnikov.e@gmail.com>
2023-04-23 12:49:45 +0300

    1

--- test_def.py
+++ test_def.py
@@ -11,18 +11,18 @@ def t_def():
     df = pd.read_csv('data.csv')
 
     # Создадим переменную save_male и запишем в неё отфильтрованную таблицу по значениям из задания.
-    save_male = df[df['Sex'] == "male"]
+    male = df[df['Sex'] == "male"]
 
     # Выберем нужные столбцы.
     columns = ['Name', 'Age', 'Pclass']
 
     # Удалим ненужные столбцы из отфильтрованной таблицы
-    save_male = pd.DataFrame(data=save_male, columns=columns)
+    male = pd.DataFrame(data=male, columns=columns)
 
     # Вывести Pclass, Name, Age спасённых
     x, y = st.slider("Задайте диапазон возраста", 0, 90, (30, 60))
-    save_male_filter = save_male[save_male['Age'].between(x, y)]
-    st.write(save_male_filter)
+    male_filter = male[male['Age'].between(x, y)]
+    st.write(male_filter)
 
     if st.checkbox('Показать список мужчин без указания возраста'):
-        st.write(save_male[save_male['Age'].isna()])
+        st.write(male[male['Age'].isna()])
