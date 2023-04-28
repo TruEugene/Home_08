bc31a96d2025a0ab5c1afe603fccd7250bb1b498
Trubnikov Eugene <trubnikov.e@gmail.com>
2023-04-28 19:45:19 +0300

    change def

--- test_def.py
+++ test_def.py
@@ -31,7 +31,7 @@ def t_def():
         st.success("Доля спасенных пассажиров Титаника")
         st.write("Моложе 30: ", round((surv30 / count) * 100, 2),
                  "Старше 60: ", round((surv60 / count) * 100, 2))
-    else:
+    elif choice == "Погибших":
         st.error("Доля погибших пассажиров Титаника")
         st.write("Моложе 30: ", round((unsurv30 / count) * 100, 2),
                  "Старше 60: ", round((unsurv60 / count) * 100, 2))
