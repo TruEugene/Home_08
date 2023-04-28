16aaffc982f1fdcc92d19506f92d19950274add7
Trubnikov Eugene <trubnikov.e@gmail.com>
2023-04-28 19:06:35 +0300

    del Title

--- urin_def.py
+++ urin_def.py
@@ -6,21 +6,22 @@ def ilya_def():
     with open("data.csv") as file:
         avg30 = 0
         avg60 = 0
+        unsurv30 = 0
+        unsurv60 = 0
         for line in file:
             survived = line.rstrip().split(",")
+            unsurvived = line.rstrip().split(",")
             if survived[1] == "1":
                 if survived[6] < "30":
                     avg30 += 1
-                    if survived[6] > "60":
-                        avg60 += 1
+                if survived[6] > "60":
+                    avg60 += 1
+            if unsurvived[1] == "0":
+                if unsurvived[6] < "30":
+                    unsurv30 += 1
+                if survived[6] > "60":
+                    unsurv60 += 1
 
-                        unsurv30 = 0
-                        unsurv60 = 0
-                        for line in file:
-                            unsurvived = line.rstrip().split(",")
-                            if unsurvived[1] == "0":
-                                if unsurvived[6] < "30":
-                                    unsurv30 += 1
     choice = st.radio("Среди кого вести поиск?", ["среди спасенных", "среди погибших"])
     if choice == "среди спасенных":
         st.success("Спасенные пассажиры Титаника")
