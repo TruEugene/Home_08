4bca92ba1df55ed33a20dfac94b84ac864ad65ca
Trubnikov Eugene <trubnikov.e@gmail.com>
2023-04-28 19:20:40 +0300

    add code

--- urin_def.py
+++ urin_def.py
@@ -4,20 +4,20 @@ import streamlit as st
 def ilya_def():
     st.header("Посчитать долю пассажиров Титаника, указав: вести поиск среди спасенных или погибших")
     with open("data.csv") as file:
-        avg30 = 0
-        avg60 = 0
+        surv30 = 0
+        surv60 = 0
         unsurv30 = 0
         unsurv60 = 0
         for line in file:
             survived = line.rstrip().split(",")
-            unsurvived = line.rstrip().split(",")
+#            unsurvived = line.rstrip().split(",")
             if survived[1] == "1":
                 if survived[6] < "30":
-                    avg30 += 1
+                    surv30 += 1
                 if survived[6] > "60":
-                    avg60 += 1
-            if unsurvived[1] == "0":
-                if unsurvived[6] < "30":
+                    surv60 += 1
+            if survived[1] == "0":
+                if survived[6] < "30":
                     unsurv30 += 1
                 if survived[6] > "60":
                     unsurv60 += 1
@@ -25,7 +25,7 @@ def ilya_def():
     choice = st.radio("Среди кого вести поиск?", ["среди спасенных", "среди погибших"])
     if choice == "среди спасенных":
         st.success("Спасенные пассажиры Титаника")
-        st.write("Моложе 30: ", avg30, "Старше 60: ", avg60)
+        st.write("Моложе 30: ", surv30, "Старше 60: ", surv60)
     else:
         st.error("Погибшие пассажиры Титаника")
         st.write("Моложе 30: ", unsurv30, "Старше 60: ", unsurv60)
