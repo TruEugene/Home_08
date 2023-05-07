6968bde79bef7d21d184291bf192b4b4ecee97fb
Mariya <masha@43.ru>
2023-05-07 13:27:17 +0300

    new code

--- parhacheva_def.py
+++ parhacheva_def.py
@@ -1,20 +1,19 @@
 import streamlit as st
-
+import pandas as pd
 def pass_list(data, save):
-    titanic = []
-    for line in data:
-        if line.split(',')[10] != "" and line.split(',')[10] == "0" and line.split(',')[1] == save:
-            titanic += [line]
-    return (titanic)
-
+    data_fare = data[(data['Плата за проезд'] == 0)]
+    out_data = data_fare[(data_fare['Спасен (да/нет)'] == save)]
+    return (out_data)
 def mariya_def():
-    with open("data.csv") as data_file:
-        data = data_file.readlines()[1:]
-    st.title('Список пассажиров Титаника с нулевой стоимостью билета')
-    radio = st.radio("Спасен?", ['Да', 'Нет'])
-    if radio == 'Да':
-        save = '1'
+    st.header('Данные пассажиров с билетом нулевой стоимости')
+    data = pd.read_csv('data.csv', delimiter = ',')
+    data.columns = ['Ид пассажира', 'Спасен (да/нет)', 'Класс каюты', 'ФИО', 'Пол', 'Возраст',
+                    'Количество родственников на борту', 'Количество родителей/детей на борту',
+                    'Номер билета', 'Плата за проезд', 'Каюта', 'Порт посадки']
+    radio = st.radio ('Вас интересует список:', ['Спасен', 'Не спасен'])
+    if radio == 'Спасен':
+        save = 1
     else:
-        save = '0'
+        save = 0
     st.dataframe(pass_list(data, save))
 
