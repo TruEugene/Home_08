87ab43b8687d1774b42cfa2b5b30c4c366975495
Trubnikov Eugene <trubnikov.e@gmail.com>
2023-05-09 23:16:23 +0300

    change test

--- trubnikov_def.py
+++ trubnikov_def.py
@@ -1,10 +1,10 @@
 import streamlit as st
 import pandas as pd
 
-data = pd.read_csv('data.csv')
+df = pd.read_csv('data.csv')
 
 
-def eugene_def(n):
+def eugene_def(data, n):
     # Создадим переменную saved и запишем в неё отфильтрованную таблицу
     saved = data[data['Survived'] == 1]
 
@@ -17,3 +17,8 @@ def eugene_def(n):
     # Вывести Pclass, Name, Age спасённых с именами начинающихся на введённый текст
     out = saved[saved['Name'].str.startswith(n)]
     return out
+
+
+st.header("Поиск спасённых пассажиров Титаника")
+name = st.text_input("Введите первые буквы фамилии")
+st.write(eugene_def(df, name))
--- trubnikov_def_test.py
+++ trubnikov_def_test.py
@@ -2,25 +2,32 @@ import pandas as pd
 from trubnikov_def import eugene_def
 
 
-def test_eugene_def():
-    # Создаем тестовый файл data.csv
+def test_eugene_def(monkeypatch):
+    # Создаем тестовый DataFrame
     data = pd.DataFrame({
-        'Survived': [1, 0, 0],
+        'Survived': [1, 0, 1],
         'Pclass': [1, 2, 3],
-        'Name': ['Trubnikov', 'John', 'Trujennikov'],
+        'Name': ['Eugene', 'John', 'Jane'],
         'Age': [20, 30, 40]
     })
-    data.to_csv('data.csv', index=False)
+
+    # Функция-заглушка для ввода текста от пользователя
+    def mock_input(prompt):
+        return 'E'
+
+    # Замена встроенной функции input() на функцию-заглушку
+    monkeypatch.setattr('builtins.input', mock_input)
 
     # Вызов функции eugene_def с тестовыми данными
-    result = eugene_def('Tr')
+    result = eugene_def(data, 'E')
 
     # Ожидаемый результат
     expected_result = pd.DataFrame({
         'Pclass': [1],
-        'Name': ['Trubnikov'],
+        'Name': ['Eugene'],
         'Age': [20]
     })
 
     # Проверка результатов
     pd.testing.assert_frame_equal(result, expected_result)
+
