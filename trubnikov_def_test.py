aa404bdb0e92e2cf2f24d235d73016b513bb840b
Trubnikov Eugene <trubnikov.e@gmail.com>
2023-05-09 23:25:51 +0300

    add test

--- trubnikov_def_test.py
+++ trubnikov_def_test.py
@@ -5,26 +5,26 @@ from trubnikov_def import eugene_def
 def test_eugene_def(monkeypatch):
     # Создаем тестовый DataFrame
     data = pd.DataFrame({
-        'Survived': [1, 0, 1],
+        'Survived': [1, 1, 0],
         'Pclass': [1, 2, 3],
-        'Name': ['Eugene', 'John', 'Jane'],
+        'Name': ['Trubnikov', 'Ivanov', 'Truschin'],
         'Age': [20, 30, 40]
     })
 
     # Функция-заглушка для ввода текста от пользователя
     def mock_input(prompt):
-        return 'E'
+        return 'Tr'
 
     # Замена встроенной функции input() на функцию-заглушку
     monkeypatch.setattr('builtins.input', mock_input)
 
     # Вызов функции eugene_def с тестовыми данными
-    result = eugene_def(data, 'E')
+    result = eugene_def(data, 'Tr')
 
     # Ожидаемый результат
     expected_result = pd.DataFrame({
         'Pclass': [1],
-        'Name': ['Eugene'],
+        'Name': ['Trubnikov'],
         'Age': [20]
     })
 
