91f2b8c8c6db399f90bc6e08a2893a96d1d2fd74
OgreMageLucky <strug2012@rambler.ru>
2023-05-21 22:11:03 +0300

    Доработано с учетом рекомендаций flake8, обновлен код

--- malashkin_def_test.py
+++ malashkin_def_test.py
@@ -1,4 +1,5 @@
 import pandas as pd
+import streamlit as st
 from malashkin_def import artem_def
 
 
@@ -32,7 +33,7 @@ def test_filter_male():
     assert (filtered_data['Sex'] == 'male').all()
 
 
-def test_data_3():
+def test_filter_age():
     # Создадим тестовый DataFrame с одним мужчиной и одной женщиной,
     # возраст которых находится в диапазоне [30, 60]
     return pd.DataFrame({
@@ -41,3 +42,33 @@ def test_data_3():
         'Sex': ['male', 'female'],
         'Pclass': [1, 2]
     })
+
+
+def test_artem_def():
+    # Создаем тестовый DataFrame с данными
+    data = pd.DataFrame({
+        'Name': ['John Smith', 'Michael Brown', 'Emma Johnson'],
+        'Age': [35, 45, 55],
+        'Sex': ['male', 'male', 'female'],
+        'Pclass': [1, 2, 3]
+    })
+
+    # Заглушка для streamlit.write
+    def mock_write(data):
+        assert len(data) == 2  # Ожидаем 2 строки
+        assert (data['Sex'] == 'male').all()
+
+    # Подменяем streamlit.write
+    original_write = st.write
+    st.write = mock_write
+
+    # Подменяем pd.read_csv
+    original_read_csv = pd.read_csv
+    pd.read_csv = lambda _: data
+
+    # Вызываем функцию artem_def
+    artem_def()
+
+    # Восстанавливаем оригинальные функции
+    st.write = original_write
+    pd.read_csv = original_read_csv
