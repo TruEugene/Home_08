9ad4ec6c9003d7ce5bdd2e9c294ccac0f0958c5d
TruEugene <nonexistenman@gmail.com>
2023-05-29 11:03:29 +0300

    Add Urin_def_test

--- urin_def.py
+++ urin_def.py
@@ -1,35 +1,43 @@
 import streamlit as st
 
 
-def ilya_def():
-    st.header("Доля пассажиров Титаника среди спасенных и погибших")
-    with open("data.csv") as file:
-        surv30 = 0
-        surv60 = 0
-        unsurv30 = 0
-        unsurv60 = 0
-        count = -1
-        for line in file:
-            data = line.rstrip().split(",")
-            count += 1
-            if data[1] == "1":
-                if data[6] < "30":
-                    surv30 += 1
-                if data[6] > "60":
-                    surv60 += 1
-            if data[1] == "0":
-                if data[6] < "30":
-                    unsurv30 += 1
-                if data[6] > "60":
-                    unsurv60 += 1
+def ilya_def(data, save):
+    #    unsurv60 = 0
+    #    unsurv30 = 0
+    avg30 = 0
+    avg60 = 0
+    #    min30 = "Моложе 30 - "
+    #    max60 = "Старше 60 - "
+    #   with open("data.csv") as file:
+    for line in data:
+        survived = line.rstrip().split(",")
+        if line.split(",")[0] == "PassengerId":
+            continue
+        if int(survived[1]) == int(save):
+            if survived[6] < "30":
+                avg30 += 1
+            if survived[6] > "60":
+                avg60 += 1
+            # if survived[1] == "0":
+            #   if survived[6] < "30":
+            #      unsurv30 += 1
+            # if survived[6] > "60":
+            #    unsurv60 += 1
+        # choice = st.radio ("Среди кого вести поиск?", ["среди спасенных", "среди погибших"])
+        # if choice == "среди спасенных":
+        #   st.success("Спасенные пассажиры Титаника")
+        avg1 = (str(avg30) + ", ")
+        avg2 = (str(avg60))
+        avg = avg1 + avg2
+    return avg
+    # st.text(avg)
+    ##   st.warning("Погибшие пассажиры Титаника")
+    # uavg1 = (min30 + str(unsurv30) + ", ")
+    # uavg2 = (max60 + str(unsurv60))
+    #        uavg = uavg1 + uavg2
+    #  st.text(uavg)
 
-    choice = st.radio("Вести поиск среди:", ["Спасенных", "Погибших"])
 
-    if choice == "Спасенных":
-        st.success("Доля спасенных пассажиров Титаника")
-        st.write("Моложе 30 - ", round((surv30 / count) * 100, 2), "%")
-        st.write("Старше 60 - ", round((surv60 / count) * 100, 2), "%")
-    elif choice == "Погибших":
-        st.error("Доля погибших пассажиров Титаника")
-        st.write("Моложе 30 - ", round((unsurv30 / count) * 100, 2), "%")
-        st.write("Старше 60 - ", round((unsurv60 / count) * 100, 2), "%")
+with open("data.csv") as file:
+    data = file.readlines()
+print(ilya_def(data, 1))
